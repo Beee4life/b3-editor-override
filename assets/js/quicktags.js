217d65ed50e6b255c32a63ba2ba541fc42332445
Berry Plasman <berry@totaldesign.com>
2020-06-09 00:46:31 +0200

    Remove second pre

--- assets/js/quicktags.js
+++ assets/js/quicktags.js
@@ -1,2 +1,1 @@
 edButtons[edButtons.length] = new edButton( 'pre', 'pre', '<pre lang="php" line="1" escaped="true">', '</pre>' );
-edButtons[edButtons.length] = new edButton( 'pre2', '<pre></pre>', '<pre lang="php" line="1" escaped="true"></pre>', '' );
